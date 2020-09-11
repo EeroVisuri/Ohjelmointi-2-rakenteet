64bde945c8083ac172ad9808f9163ce776d42a2a
Eero Visuri <kneevi@linux-desktop1.tuni.fi>
2020-09-11 20:41:38 +0300

    Korjattu typo

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -9,10 +9,10 @@ int main()
     int temperature;
     cin >> temperature;
     float fahrenheit = temperature * 1.8 + 32;
-    float celcius = (temperature - 32) / 1.8;
+    float celsius = (temperature - 32) / 1.8;
 
     cout << temperature << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit" <<endl;
-    cout << temperature << " degrees Fahrenheit is " << celcius << " degrees Celcius" <<endl;
+    cout << temperature << " degrees Fahrenheit is " << celsius << " degrees Celsius" <<endl;
 
     return 0;
 }
