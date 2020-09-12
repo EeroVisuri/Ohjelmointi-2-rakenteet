1a95e50e0ce2723e95570f40b2d060fb42b7fc9a
Eero Visuri <kneevi@linux-desktop10.tuni.fi>
2020-09-12 16:25:45 +0300

    Finalized the program, it should now work accoding to specifications.

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -8,11 +8,15 @@ int main()
     std::cin >> number; //lets put the number user gave us in the variable
     int i;
     for (i=1; i<=number;) { //this loop prints out numbers and zips and boings
-        if (i%3==0) {  //if variable i is divisible by 3, we print zip instead of the number
+        if (i%3==0 && i%7==0) { //if variable i is divisible by both 3 and 7, we print zip boing
+            cout << "zip boing"<<endl;
+            i++;
+        }
+        else if (i%3==0) {  //if variable i is divisible by 3, we print zip instead of the number
             cout << "zip"<<endl;
             i++;
         }
-        if (i%7==0) { //if i is divisible by 7, we print boing instead of the number
+        else if (i%7==0) { //if i is divisible by 7, we print boing instead of the number
             cout << "boing"<<endl;
             i++;
         }
