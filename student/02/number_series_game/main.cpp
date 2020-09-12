513cad9a39e71308ae1b4faabe802e48c910b56b
Eero Visuri <kneevi@linux-desktop10.tuni.fi>
2020-09-12 16:07:12 +0300

    Number game prints out the numbers we wanted.

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
+    int number; //variable to store the number from user in
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> number; //lets put the number user gave us in the variable
+    int i;
+    for (i=1; i<=number;) { //this loop prints out numbers from 1 to the number stored in variable int number.
+        cout << i<<endl;
+        i++;
+    }
 }
