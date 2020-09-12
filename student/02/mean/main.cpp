768688eec5356658bf143aa2a744edec1ff0b608
Eero Visuri <kneevi@linux-desktop10.tuni.fi>
2020-09-12 16:45:47 +0300

    Structure of mean calculator done, next up is making it work.

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-
+using namespace std;
 // Write here a function counting the mean value
+float meanCalculator(int integers) {
+    float mean;
+    for (int var = 0; var < integers; ++var) {
+
+    }
+    return mean;
+}
 
 int main()
 {
+    int integers;
     std::cout << "From how many integer numbers you want to count the mean value? ";
+    cin >> integers;
+
+    if (integers >0 ) {
+        meanCalculator(integers);
+    }
+    else {
+        cout << "Cannot count mean value from "<<integers;
+    }
 }
