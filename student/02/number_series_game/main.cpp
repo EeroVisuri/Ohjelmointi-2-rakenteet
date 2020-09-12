6562b8ac6e9599ed032129f6f1f307d6c66e71c2
Eero Visuri <kneevi@linux-desktop10.tuni.fi>
2020-09-12 16:13:31 +0300

    Added processing numbers that are divisible by 3.

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -7,8 +7,14 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     std::cin >> number; //lets put the number user gave us in the variable
     int i;
-    for (i=1; i<=number;) { //this loop prints out numbers from 1 to the number stored in variable int number.
-        cout << i<<endl;
-        i++;
+    for (i=1; i<=number;) { //this loop prints out numbers and zips and boings
+        if (i%3==0) {  //if variable i is divisible by 3, we print zip instead of the number
+            cout << "zip"<<endl;
+            i++;
+        }
+        else { //in any other case we print out the current number
+            cout << i<<endl;
+            i++;
+        }
     }
 }
