978a683d780478b76fe658e0eff477f264fa47fc
Eero Visuri <kneevi@linux-desktop4.tuni.fi>
2020-09-13 18:14:28 +0300

    Cube with working overflow detection.

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <climits>
+#include <cstdint>
 using namespace std;
 
 
 
 //if this method returns 1, you have overflowed. It will return 0 otherwise.
 int overFlowcheck(int usersnnumber, int result) {
+    if (usersnnumber < INT_MAX || result < INT_MAX) {
+        return 1;
+    }
     if (usersnnumber > 0 && result > 0) {
         return 0;
     }
     if (usersnnumber < 0 && result < 0) {
         return 0;
-    }
-    if (usersnnumber < INT_MAX && result < INT_MAX) {
-        return 1;
     }
     else {
         return 1;
@@ -26,9 +27,9 @@ int main() {
     int usersnumber;
     cout << "Enter a number: " <<endl;
     cin >> usersnumber;
-
+    //doing the calculations
     int result = usersnumber*usersnumber*usersnumber;
-
+    //checking if we overflowed
     if (overFlowcheck(usersnumber, result) > 0) {
         cout << "Error! The cube of " << usersnumber << " is not " << result << "." << endl;
     }
