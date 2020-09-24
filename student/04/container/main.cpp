aa30d7bc4121da456265b9c4f074d36bb4b2619c
Eero Visuri <kneevi@linux-desktop5.tuni.fi>
2020-09-24 17:02:13 +0300

    Fixed some warnings caused by vector.size() usage in for-loops.

--- student/04/container/main.cpp
+++ student/04/container/main.cpp
@@ -16,7 +16,8 @@ void read_integers(std::vector< int >& ints, int count)
 
 // Returns true if all integers in a vector are the same
 bool same_values(std::vector<int>integers) {
-    for (int var = 1; var < integers.size(); ++var) {
+    int n = integers.size();
+    for (int var = 1; var < n; ++var) {
         int previndex = integers.at(var-1);
         if (previndex != integers.at(var)) {
             return false;
@@ -29,7 +30,8 @@ bool same_values(std::vector<int>integers) {
 
 // returns true if all integers in a vector are ascending (non-strict so identical values are allowed)
 bool is_ordered_non_strict_ascending (std::vector<int>integers) {
-    for (int var = 1; var < integers.size(); ++var) {
+    int n = integers.size();
+    for (int var = 1; var < n; ++var) {
         int previndex = integers.at(var-1);
         if (previndex > integers.at(var)) {
             return false;
@@ -41,10 +43,11 @@ bool is_ordered_non_strict_ascending (std::vector<int>integers) {
 
 // returns true if difference between consecutive terms in the vector is constant
 bool is_arithmetic_series (std::vector<int>integers) {
+    int n = integers.size();
     //setting up a variable where to store the difference between consecutive elements
     int diff = integers[1] - integers[0];
     //for-loop to check if there's a difference at some point
-    for (int var = 2; var < integers.size(); ++var) {
+    for (int var = 2; var < n; ++var) {
         if (integers[var] - integers[var-1] != diff) {
             return false;
         }
@@ -55,10 +58,11 @@ bool is_arithmetic_series (std::vector<int>integers) {
 
 // returns true if numbers in a vector have constant ratio between consecutive integers
 bool is_geometric_series (std::vector<int>integers) {
+    int n = integers.size();
     //checking ratio
     int ratio = integers[1] / integers[0];
     //for-loop to check if ratio remains
-    for (int var = 2; var < integers.size(); ++var) {
+    for (int var = 2; var < n; ++var) {
         if (integers[var] / integers[var-1] != ratio) {
             return false;
         }
