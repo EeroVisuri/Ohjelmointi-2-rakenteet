b7c9ea487d1484ce2527b879ccf5775e4a67181e
Eero Visuri <kneevi@linux-desktop10.tuni.fi>
2020-10-01 17:49:21 +0300

    Last try before moving on.

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 
 
-int main()
-{
+
+int main() {
 
     string inputfile = "";
     string outputfile = "";
@@ -25,18 +25,18 @@ int main()
     //Creating the file_object stream to read from the file with given name
 
     ifstream file_object;
-
-
     ofstream file_object_for_output;
+
+    //ofstream file_object(outputfile);
 
 
     //if we can't read from inputfile, print out an error
-    if (not file_object) {
+    if (!file_object) {
         cout << "Error! The file " << inputfile << " cannot be opened." << endl;
         return EXIT_FAILURE;
     }
+    //Else while the file has lines, we read those lines and put them in a variable
     else {
-
         string line;
         int linenumber = 1;
         file_object.open(inputfile);
@@ -49,5 +49,7 @@ int main()
         file_object.close();
         file_object_for_output.close();
 
+
+        cout << "End of program, linenumber was: " << linenumber << endl;
     }
 }
