dbc98fbec3de22c445b955f4ea7d0b22a791c6e2
Eero Visuri <kneevi@linux-desktop9.tuni.fi>
2020-10-29 22:49:45 +0200

    Fixed a printout.

--- student/08/student_register/main.cpp
+++ student/08/student_register/main.cpp
@@ -141,6 +141,7 @@ int main() {
                 continue;
             }
             std::cout << "Enter a new phone number: ";
+            std:: cout << std::endl;
             std::string new_number;
             std::getline (std::cin, new_number);
             if (!is_valid_phone_number(new_number)) {
